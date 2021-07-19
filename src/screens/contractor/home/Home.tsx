aec271141ef66b8a23195df9aef7a86c68e01ca9
nachoviejo <ignacionachoviejo@gmail.com>
2021-07-18 21:25:16 -0300

    contractor home screen

--- src/screens/contractor/home/Home.tsx
+++ src/screens/contractor/home/Home.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
 
-import { makeStyles, Theme } from '@material-ui/core';
+import { makeStyles, Theme, Typography } from '@material-ui/core';
+import { Card } from '@material-ui/core';
+import DoneIcon from '@material-ui/icons/Done';
+import CloseIcon from '@material-ui/icons/Close';
+import TimerIcon from '@material-ui/icons/Timer';
 
 import { drawerWidth, headerSize } from '../../../utils/constants';
+import colors from '../../../utils/styles/globalColors';
+import barras from '../../../assets/images/barras.PNG';
 
 const useStyles = makeStyles((theme: Theme) => ({
     container:{
         marginTop: headerSize,
         marginLeft: drawerWidth,
-        backgroundColor: '#808080',
+    },
+    cardContainer:{
+        display: 'flex',
+        flex: 1,
+    },
+    card:{
+        flex: 1,
+        marginTop: '10%',
+        marginInline: '10%',
+    },
+    counterContainer:{
+        display: 'flex',
+        flex: 1,
+        backgroundColor: colors.lightBlue,
+        justifyContent: 'center',
+        textAlign: 'center',
+        
+    },
+    imgContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        margin:'auto',
+    },
+    img:{
+        width: 120,
+    },
+    counter:{
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        paddingTop: '15%',
+        paddingBottom: '15%',
+    },
+    counterText:{
+        color: colors.white,
+    },
+    details:{
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        paddingTop: '10%',
+        paddingBottom: '10%',
+    },
+    row:{
+    },
+    done:{
+        color: colors.green,
+    },
+    wrong:{
+        color: colors.red,
+    },
+    wait: {
+        color: colors.yellow,
     },
 }));
 
@@ -18,9 +77,70 @@ const Home = () => {
 
     return (
         <div className={classes.container}>
-            <text>
-                Home
-            </text>
+            <div className={classes.cardContainer}>
+                <Card className={classes.card}>
+                    <div className={classes.counterContainer}>
+                        <div className={classes.imgContainer}>
+                            <img src={barras} className={classes.img}/>
+                        </div>
+                        <div className={classes.counter}>
+                            <Typography variant="h6" className={classes.counterText}>
+                                Total
+                            </Typography>
+                            <Typography variant="h3" className={classes.counterText}>
+                                45
+                            </Typography>
+                        </div>
+                    </div>
+                    <div className={classes.details}>
+                        <div className={classes.row}>
+                            <DoneIcon className={classes.done}/>
+                            <text>
+                                Validos
+                            </text>
+                        </div>
+                        <div className={classes.row}>
+                            <TimerIcon className={classes.wait}/>
+                            <text>
+                                Pendientes
+                            </text>
+                        </div>
+                        <div className={classes.row}>
+                            <CloseIcon className={classes.wrong}/>
+                            <text>
+                                Vencidos
+                            </text>
+                        </div>
+                    </div>
+                </Card>
+                <Card className={classes.card}>
+                    <div className={classes.counterContainer}>
+                        <div className={classes.imgContainer}>
+                            <img src={barras} className={classes.img}/>
+                        </div>
+                        <div className={classes.counter}>
+                            <Typography variant="h6" className={classes.counterText}>
+                                Total
+                            </Typography>
+                            <Typography variant="h3" className={classes.counterText}>
+                                35
+                            </Typography>
+                        </div>
+                    </div>
+                    <div className={classes.details}>
+                        <div>
+                            <text>
+                                Conductores
+                            </text>
+                        </div>
+                        <div>
+                            <text>
+                                Vehiculos
+                            </text>
+                        </div>
+                    </div>
+                </Card>
+            </div>
         </div>
     )
 }
--- src/utils/styles/globalColors.ts
+++ src/utils/styles/globalColors.ts
@@ -1,7 +1,10 @@
 const globalColors = {
     grey: '#BDBDBD',
     white: '#FFFFFF',
-    lightBlue: '#2196F3'
+    lightBlue: '#2196F3',
+    green: '#67D134',
+    red: '#F04E4E',
+    yellow: '#EDD555',
 };
 
 export default globalColors;
